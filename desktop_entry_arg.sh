9a5320fbed2150a8450309feab47c240d7f57402
pbxforce <120632049+pbxforce@users.noreply.github.com>
2023-08-21 17:03:11 +0530

    Update desktop_entry_arg.sh

--- desktop_entry_arg.sh
+++ desktop_entry_arg.sh
@@ -1,11 +1,34 @@
 #!/bin/bash
 
 # This script is for creating desktop entry of applications from run file.
-# Syntax is: desktop_entry.sh <set-name> <path-to-app-run-file> <path-to-app-favicon-file> <username>
+# Help parameters
+help_section() {
+    echo
+    echo "--------------Linux Desktop Entry--------------"
+    echo
+    echo "Sytax: sudo bash desktop_entry_arg.sh <set-name> <executable-file-absolute-path> <path-to-app-favicon-file> <username>"
+    cat <<EOF
 
-echo "Creating desktop entry..."
+- set-name: Name for the desktop application
+- executable-file-absolute-path: Full path of application executable file. Usually It's .run file
+- favicon-file-absolute-path: Full path of favicon file. Usually It's located within the applicaiton installation directory. Use .ico file only
+- system-username: Name of the user who will be using this desktop entry
+
+EOF
+}
+
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]];then
+    help_section
+    exit 0
+fi
 shortcut_entry="/usr/share/applications/$1.desktop"
 touch $shortcut_entry
+if [[ -e $shortcut_entry ]];then
+    echo "Desktop entry created"
+else
+    echo "Unable to create desktop entry"
+    exit 1
+fi
 cat <<EOF> $shortcut_entry
 [Desktop Entry]
 Version=1.0
@@ -16,6 +39,16 @@ Icon=$3
 Terminal=false
 StartupNotify=false
 EOF
-echo "Assigning permissions..."
+if [[ `cat /usr/share/applications/xampp.desktop|grep Name|cut -c6-` == "$1" ]];then
+    echo "Entry file written successsfully"
+else
+    echo "Entry file could not be written"
+    exit 1
+fi
 echo "$4 ALL=NOPASSWD: $2" >> /etc/sudoers
-echo "DONE"
+if [[ `cat /etc/sudoers|tail -n 1|awk '{print$1}'` == "$4" ]];then
+    echo "DONE!!"
+else
+    echo "Unable to add permission for user"
+    exit 1
+fi
