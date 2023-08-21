7074b407156292a0ca9a1908e284070a5cd3ba5c
pbxforce <120632049+pbxforce@users.noreply.github.com>
2023-08-21 17:15:33 +0530

    Update desktop_entry_inp.sh

--- desktop_entry_inp.sh
+++ desktop_entry_inp.sh
@@ -5,14 +5,12 @@
 help_section() {
     echo
     echo "--------------Linux Desktop Entry--------------"
-    echo
-    echo "Sytax: sudo bash desktop_entry_arg.sh <set-name> <executable-file-absolute-path> <path-to-app-favicon-file> <username>"
     cat <<EOF
 
-- set-name: Name for the desktop application
-- executable-file-absolute-path: Full path of application executable file. Usually It's .run file
-- favicon-file-absolute-path: Full path of favicon file. Usually It's located within the applicaiton installation directory. Use .ico file only
-- system-username: Name of the user who will be using this desktop entry
+- Desktop entry name: Name for the desktop application
+- Executable file path: Absolute path of application executable file. Usually It's .run file
+- Favicon file path: Absolute path of favicon file. Usually It's located within the applicaiton installation directory. Use .ico file only
+- system username: Name of the user who will be using this desktop entry
 
 EOF
 }
